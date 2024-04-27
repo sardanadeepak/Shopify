c478dfbde06d409e206b7b98c4d52a301ae0e313
Deepak Sardana <deepaksardana03@gmail.com>
2024-04-26 21:17:48 -0400

    updates

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 router.get("/", async (req, res) => {
-    const filepath = path.join(__dirname, 'data.txt');
+    const filepath = path.join(__dirname, './data.txt');
     fs.readFile(filepath, 'utf8', (err, data) => {
         if (err) {
             return res.status(404).send('File not found');
